9b9fce735b6fec4808274c533834b18c4149d40f
pedro_braz_s <132957475+Pedrobbs@users.noreply.github.com>
2024-02-20 14:09:03 -0300

    Código hello java

---
+++ Ola Mundo/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Java!!!");
+
+        int x = 10;
+        int y = 20;
+
+        System.out.println("Soma: " +(x+y));
+
+    }
+}

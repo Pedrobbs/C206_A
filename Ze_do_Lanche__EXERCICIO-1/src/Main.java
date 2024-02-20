0b3cfa35fa38010b5980a9aed73d934753ca9e43
pedro_braz_s <132957475+Pedrobbs@users.noreply.github.com>
2024-02-20 14:39:56 -0300

    Exercício Trailer do Zé

---
+++ Ze_do_Lanche__EXERCICIO-1/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+    int lanche1 = 10;
+    int lanche2 = 4;
+    int lanche3 = 2;
+
+    int total = (lanche1+lanche2+lanche3);
+    float media = (total/3.0f);  
+
+    System.out.println("Total: "+ total);
+    System.out.println("Media: "+ media);
+
+    }
+}

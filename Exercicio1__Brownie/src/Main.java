4de9d127406b47b4a06296b9ce75b410ba2c2e80
pedro_braz_s <132957475+Pedrobbs@users.noreply.github.com>
2024-04-05 14:25:07 -0300

    Exercicio1__Brownie

---
+++ Exercicio1__Brownie/src/Main.java
@@ -0,0 +1,36 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BrownieNutella bn = new BrownieNutella("Brownie", 20, "Nutella");
+        BrownieCafe bc = new BrownieCafe("Brownie", 10, "Café");
+        BrownieDoceDeLeite bl = new BrownieDoceDeLeite("Brownie", 15, "Doce De Leite");
+
+        // Adicionando complementos ao Brownie
+        bn.adicionaNutella();
+        bc.adicionaCafe();
+        bl.adicionaDoceDeLeite();
+
+        System.out.println();
+
+        // Adicionando os Brownies ao carrinho
+        bn.addCarrinhoDeCompras();
+        bc.addCarrinhoDeCompras();
+        bl.addCarrinhoDeCompras();
+
+        System.out.println();
+
+        // Calculando os total gasto em brownie
+        bn.calculaValorTotal();
+        bc.calculaValorTotal();
+        bl.calculaValorTotal();
+
+        System.out.println();
+
+        // Mostrando as informações de cada Brownie
+        bn.mostraInfo();
+        System.out.println();
+        bc.mostraInfo();
+        System.out.println();
+        bl.mostraInfo();
+    }
+}

1bf99cfcb9de20be41fccae4ea66a27035c66683
pedro_braz_s <132957475+Pedrobbs@users.noreply.github.com>
2024-04-23 14:02:50 -0300

    Exercicio_Interface

---
+++ Exercicio_Interface/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Cachorro cachorro = new Cachorro("Cachorro", 32);
+        Boi boi = new Boi("Boi", 50);
+        Lontra lontra = new Lontra("Lontra", 20);
+
+        System.out.println("Sons de cachorro, boi e lontra: ");
+        cachorro.emitirSom();
+        boi.emitirSom();
+        lontra.emitirSom();
+
+        System.out.println();
+
+        System.out.println("Animal Aquatico: ");
+        lontra.nadar();
+
+        System.out.println();
+
+        System.out.println("Info Cachorro: ");
+        cachorro.mostraInfo();
+        System.out.println();
+        System.out.println("Info Boi: ");
+        boi.mostraInfo();
+        System.out.println();
+        System.out.println("Info Lontra: ");
+        lontra.mostraInfo();
+
+
+    }
+}

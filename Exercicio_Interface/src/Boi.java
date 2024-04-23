71fbb047661a19d07d9b20a489f8f652425452f9
pedro_braz_s <132957475+Pedrobbs@users.noreply.github.com>
2024-04-23 14:02:13 -0300

    Delete Exercicio_Interface directory

--- Exercicio_Interface/src/Boi.java
+++
@@ -1,5 +0,0 @@
-public class Boi extends Mamifero {
-    public Boi(String nome, double vida) {
-        super(nome, vida);
-    }
-}
--- Exercicio_Interface/src/Cachorro.java
+++
@@ -1,5 +0,0 @@
-public class Cachorro extends Mamifero {
-    public Cachorro(String nome, double vida) {
-        super(nome, vida);
-    }
-}
